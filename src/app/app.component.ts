360a3e33812ebd15ab63ea6e9793511f0b82db02
VakoNarimanidze <119787310+VakoNarimanidze@users.noreply.github.com>
2025-01-30 19:59:46 +0400

    web design is done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,47 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+interface Translations {
+  [key: string]: { 
+    [key: string]: string;
+  };
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MyPortfolio';
+  translations: Translations = {
+    en: {
+      WELCOME: "Welcome to my portfolio",
+      ABOUT_ME: "About Me",
+      ABOUT_TEXT: "My name is Valeri Narimanidze, I'm 19 years old and a front-end developer based in Tbilisi/Georgia. I have developed many types of projects from the well-known Academy 'It Step Academy'. I have experience working with HTML, CSS, SASS, JavaScript and I'm currently learning Angular with TypeScript. To see my projects, visit my Github or check out the projects below",
+      PROJECTS: "Projects",
+      STAR_HOTEL: "Star Hotel With API",
+      REPOSITORY: "Repository",
+      WEBSITE: "Website",
+      CONTACT:"Contact Me"
+    },
+    ka: {
+      WELCOME: "მოგესალმებით ჩემს პორტფოლიოში",
+      ABOUT_ME: "ჩემ შესახებ",
+      ABOUT_TEXT: "ჩემი სახელი არის ვალერი ნარიმანიძე, ვარ 19 წლის  და Front-End დეველოპერი თბილისიდან/საქართველო. მაქვს ბევრი სახის პროექტები ცნობილი 'It Step Academy'-დან. მაქვს გამოცდილება HTML, CSS, SASS, JavaScript-ში და ამჟამად ვსწავლობ Angular-ს TypeScript-ით.  ჩემი პროექტების სანახავად, ეწვიეთ ჩემს Github-ს ან ქვემოთ მოცემულ პროექტებს",
+      PROJECTS: "პროექტები",
+      STAR_HOTEL: "სტარჰოტელი API-ით",
+      REPOSITORY: "რეპოზიტორია",
+      WEBSITE: "ვებგვერდი",
+      CONTACT:  "დამეკონტაქტე"
+    }
+  };
+
+  currentLanguage: 'en' | 'ka' = 'en'; 
+  
+  switchLanguage(language: 'en' | 'ka'): void {
+    this.currentLanguage = language;
+  }
+
+  translate(key: string): string {
+    return this.translations[this.currentLanguage]?.[key] || key;
+  }
 }
